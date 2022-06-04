5bbc3cefab6c3a6f902ae6d10e1d750757f048e6
Pikogoro <pikogoro>
2022-06-05 01:34:40 +0900

    Added switching between FPS view and 3rd person view.

--- Assets/BossRoom/Scripts/Client/Game/Character/CameraController.cs
+++ Assets/BossRoom/Scripts/Client/Game/Character/CameraController.cs
@@ -8,6 +8,12 @@ namespace Unity.Multiplayer.Samples.BossRoom.Visual
     {
         private CinemachineFreeLook m_MainCamera;
 
+#if P56
+        private bool m_IsFPSView = true;
+        private Transform m_CamTransform = null;
+        private GameObject m_BoneHead = null;
+#endif  // P56
+
         void Start()
         {
             AttachCamera();
@@ -34,15 +40,38 @@ namespace Unity.Multiplayer.Samples.BossRoom.Visual
             cmCameraPrefab.SetActive(false);
 
             // Change main camera from 3rd person view to FPS view
-            Transform camTransform = Camera.main.gameObject.transform;
-            camTransform.parent = transform;
-            camTransform.localPosition = new Vector3(0f, 1.3f, 0.5f);
-            camTransform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            m_CamTransform = Camera.main.gameObject.transform;
+            m_CamTransform.parent = transform;
+            m_CamTransform.localPosition = new Vector3(0f, 1.3f, 0.5f);
+            m_CamTransform.localRotation = Quaternion.Euler(0f, 0f, 0f);
 
             // Hide character's head
-            GameObject boneHead = GameObject.Find("Bone_Head");
-            boneHead.SetActive(false);
+            m_BoneHead = GameObject.Find("Bone_Head");
+            m_BoneHead.SetActive(false);
 #endif  // P56
         }
+
+#if P56
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Backslash))
+            {
+                m_IsFPSView = !m_IsFPSView;
+
+                if (m_IsFPSView)
+                {
+                    m_CamTransform.localPosition = new Vector3(0f, 1.3f, 0.5f);
+                    m_CamTransform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                    m_BoneHead.SetActive(false);
+                }
+                else
+                {
+                    m_CamTransform.localPosition = new Vector3(0f, 3f, -5f);
+                    m_CamTransform.localRotation = Quaternion.Euler(15f, 0f, 0f);
+                    m_BoneHead.SetActive(true);    
+                }
+            }
+        }
+#endif  // P56
     }
 }
